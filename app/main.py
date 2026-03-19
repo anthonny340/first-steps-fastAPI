bb9a5eea76e4f9b72f9d996e86b5fb26a05e65c4
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-03-19 11:10:57 +0100

    chore(main.py): metodo post modificado con query parameter

--- app/main.py
+++ app/main.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI(title='Mini Blog')
 
 BLOG_POST = [
-    {'id': 1, 'title': 'Hola desde fastAPI', 'content': 'Mi primer post con fastAPI'},
-    {'id': 2, 'title': 'Segundo post desde fastAPI', 'content': 'explorando fastAPI'},
+    {'id': 1, 'title': 'Hola desde fastAPI',
+        'content': 'Mi primer post con fastAPI'},
+    {'id': 2, 'title': 'Segundo post desde fastAPI',
+        'content': 'explorando fastAPI'},
     {'id': 3, 'title': 'Tercer post desde fastAPI', 'content': 'explorando fastAPI'},
 ]
+
 
 @app.get('/')
 def home():
     return {'message': 'Bienvenidos a Mini Blog por Anthonny'}
 
+
 @app.get('/posts')
-def list_post():
+def list_post(query: str | None = Query(default=None, description='Texto para buscar por titulo')):
+    if query:
+        results = [post for post in BLOG_POST if query.lower()
+                   in post['title'].lower()]
+        # Se pude simplificar la logica con un list comprehension
+        # for post in BLOG_POST:
+        #     if query.lower() in post['title'].lower():
+        #         results.append(post)
+        return {'data': results, 'query': query}
+
     return {'data': BLOG_POST}
