a5c53ac27d86fa0f6995e9b35dbc21040ab66c0f
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-03-19 10:50:14 +0100

    chore(main.py): metodo post

--- app/main.py
+++ app/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI(title='Mini Blog')
 
+BLOG_POST = [
+    {'id': 1, 'title': 'Hola desde fastAPI', 'content': 'Mi primer post con fastAPI'},
+    {'id': 2, 'title': 'Segundo post desde fastAPI', 'content': 'explorando fastAPI'},
+    {'id': 3, 'title': 'Tercer post desde fastAPI', 'content': 'explorando fastAPI'},
+]
+
 @app.get('/')
 def home():
     return {'message': 'Bienvenidos a Mini Blog por Anthonny'}
+
+@app.get('/posts')
+def list_post():
+    return {'data': BLOG_POST}
