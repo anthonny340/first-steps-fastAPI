5cdac596196d75c4cc73ceca13789b9b282a66c2
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-03-20 15:27:59 +0100

    feat(main.py): se importo pydantic, metodos create_post y update_post modificados
    Se crearon nuevas clases que heredan de BaseModel de pydantic
    El metodo create_post se modifico el parametro y se quito validaciones
        que hacer pydantic automaticamente
    El metodo update_post se modifico el parametro y se quito hizo un refactor
        de codigo adecuandolo al tipo de dato del parametro
    Se implemento una breve documentacion de los metodos

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Query, Body, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI(title='Mini Blog')
 
@@ -11,6 +12,20 @@ BLOG_POST = [
 ]
 
 
+class PostBase(BaseModel):
+    title: str
+    content: str
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostUpdate(BaseModel):
+    title: str
+    content: str
+
+
 @app.get('/')
 def home():
     return {'message': 'Bienvenidos a Mini Blog por Anthonny'}
@@ -18,6 +33,13 @@ def home():
 
 @app.get('/posts')
 def list_post(query: str | None = Query(default=None, description='Texto para buscar por titulo')):
+    '''
+    Obtener los posts.
+
+    :param str | None query: (Query) Texto para buscar posts por titulo.
+    :return: Diccionario de posts coincidentes.
+    :rtype: (dict[str, Any] | dict[str, list[dict[str, Any]]])
+    '''
     if query:
         results = [post for post in BLOG_POST if query.lower()
                    in post['title'].lower()]
@@ -32,6 +54,14 @@ def list_post(query: str | None = Query(default=None, description='Texto para bu
 
 @app.get('/posts/{post_id}')
 def get_post(post_id: int, incluide_content: bool = Query(default=True, description='Incluir o no el contenido')):
+    '''
+    Obtener los posts por ID o por coincidencia de contenido.
+
+    :param int post_id: (Path) ID del post.
+    :param bool incluide_content: (Query) Incluir el contentenido del post.
+    :return: Diccionario de posts coincidentes.
+    :rtype: (dict[str, dict[str, Any]] | dict[str, str])
+    '''
     for post in BLOG_POST:
         if post_id == post['id']:
             if incluide_content:
@@ -43,30 +73,46 @@ def get_post(post_id: int, incluide_content: bool = Query(default=True, descript
 
 
 @app.post('/posts')
-def create_post(post: dict = Body(...)):
-    if 'title' not in post or 'content' not in post:
-        return {'error': 'Title y Content son requeridos'}
+def create_post(post: PostCreate):
+    '''
+    Crear un post.
 
-    if not str(post['title']).strip():
-        return {'error': 'Title no puede estar vacio'}
-
+    :param PostCreate data: Datos enviados para crear el post.
+    :return: Diccionario con el post actualizado.
+    :rtype: dict[str, Any]
+    '''
     new_id = (BLOG_POST[-1]['id'] + 1) if BLOG_POST else 1
     new_post = {'id': new_id,
-                'title': post['title'], 'content': post['content']}
+                'title': post.title, 'content': post.content}
 
     BLOG_POST.append(new_post)
     return {'message': 'Post creado', 'data': new_post}
 
 
 @app.put('/posts/{post_id}')
-def update_post(post_id: int, data: dict = Body(...)):
+def update_post(post_id: int, data: PostUpdate):
+    '''
+    Actualiza un post existente.
+
+    :param int post_id: ID del post a actualizar.
+    :param PostUpdate data: Datos enviados para actualizar el post.
+    :return: Diccionario con el post actualizado.
+    :rtype: dict[str, Any]
+
+    .. note::
+        Si se usa ``exclude_unset=True``, solo se actualizarán los campos
+        enviados por el usuario.
+    '''
     for post in BLOG_POST:
         if post_id == post['id']:
-            if 'title' in data:
-                post['title'] = data['title']
+            # Podemos converitir data que es tipo PostUpdate en un diccionario con la siguiente linea de codigo
+            # playload = data.model_dump(exclude_unset=True)
+            # exclude_unset=True sirve para excluir los parametros que no envia el usuario, evitando la asignacion de None a los parametros no enviados
+            if data.title:
+                post['title'] = data.title
 
-            if 'content' in data:
-                post['content'] = data['content']
+            if data.content:
+                post['content'] = data.content
 
             return {'message': 'Post actualizado', 'data': post}
 
@@ -76,6 +122,15 @@ def update_post(post_id: int, data: dict = Body(...)):
 # Este codigo quiere decir que salio bien pero no vamos a regresar nada de contenido
 @app.delete('/posts/{post_id}', status_code=204)
 def delete_post(post_id: int):
+    '''
+    Elimina un post existente.
+
+    :param int post_id: (Path) ID del post.
+    :rtype: None
+
+    .. warning::
+        Lanzara un status_code ``404``, si no se encuetra el post.
+    '''
     for index, post in enumerate(BLOG_POST):
         if post_id == post['id']:
             BLOG_POST.pop(index)
