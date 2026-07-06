2029546bf2e4f791a39bdd3f3ff0534dd33e6e12
Anthonny Sacheri <tonysolorzano880@gmail.com>
2026-07-06 15:57:35 +0200

    feat: implementacion de las funciones decode_json y get_current_user

--- app/core/security.py
+++ app/core/security.py
@@ -1,8 +1,10 @@
 import os
 from typing import Optional
 from datetime import timedelta, timezone, datetime
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
 
 # Se puede configurar un valor alternativo por defecto en caso de que no puede leer correctamente alguna varible de entorno
 # Se lo realizaria de la siguiente forma:   SECRETE_KEY = os.getenv('SECRETE_KEY', 'ALTERNATIVA_AQUI')
@@ -25,5 +27,28 @@ def create_access_token(data: dict, expire_delta: Optional[timedelta] = None):
     return token
 
 
-def decode_token():
-    pass
+def decode_token(token: str) -> dict:
+    payload = jwt.decode(jwt=token, key=SECRETE_KEY, algorithms=ALGORITHM)
+    return payload
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credential_exc = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                   detail="No autenticado", headers={"WWW-Authenticate": "Bearer"})
+
+    try:
+        payload = decode_token(token)
+        sub: Optional[str] = payload.get('sub')
+        username: Optional[str] = payload.get('username')
+        if not sub or not username:
+            raise credential_exc
+
+        return {'email': sub, 'username': username}
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token expirado",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    except InvalidTokenError:
+        raise credential_exc
